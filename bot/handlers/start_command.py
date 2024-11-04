433ba82441f7fee62f8dd4c7cc6082d44f6af251
boyfws <minkin.d.d@gmail.com>
2024-11-04 22:09:21 +0300

    Добавил логгироавние ошибок + тестим yml

--- bot/handlers/start_command.py
+++ bot/handlers/start_command.py
@@ -1,4 +1,6 @@
-from telegram import Update
+from venv import logger
+
+from telegram import Update, Message
 from telegram.ext import CallbackContext, CommandHandler
 
 from bot.buttons.menu_button_miniapp import menu_bottom_miniapp
@@ -16,8 +18,13 @@ async def start(update: Update, context: CallbackContext) -> None:
     """
     chat_id = update.effective_chat.id
     start_logger.info(f"Пришло сообщение от пользователя {update.message.from_user.id}")
-    await context.bot.set_chat_menu_button(chat_id=chat_id, menu_button=menu_bottom_miniapp)
-    await update.message.reply_text(response_text, reply_markup=start_keyboard)
+    resp_of_button_set: bool = await context.bot.set_chat_menu_button(chat_id=chat_id,
+                                                                      menu_button=menu_bottom_miniapp)
+    if not resp_of_button_set:
+        start_logger.error(f"При установке кнопки пользователю {update.message.from_user.id} возникла ошибка")
+    resp_of_mes_sent: Message = await update.message.reply_text(response_text, reply_markup=start_keyboard)
+    if resp_of_mes_sent.text != response_text:
+        start_logger.error(f"При отправке сообщения пользователю {update.message.from_user.id} возникла ошибка")
 
 
 start_command: CommandHandler = CommandHandler("start", start)
