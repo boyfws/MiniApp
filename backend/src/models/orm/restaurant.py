352ffceb36ec9f7ffb970a228aa85a422081b291
Oleg Shvetsov <oashvetsov@edu.hse.ru>
2024-11-04 21:02:54 +0300

    кажется закончил наконец с орм

--- backend/src/models/orm/restaurant.py
+++ backend/src/models/orm/restaurant.py
@@ -1,7 +1,7 @@
 import sqlalchemy as sa
 from geoalchemy2 import Geometry
 from base import BaseTable
-from sqlalchemy.orm import mapped_column, Mapped
+from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 
 class Restaurant(BaseTable):
@@ -9,10 +9,21 @@ class Restaurant(BaseTable):
     owner_id: Mapped[int] = mapped_column(sa.ForeignKey("owner.id"), nullable=False)
     name: Mapped[str] = mapped_column(sa.String, nullable=False)
     main_photo: Mapped[str] = mapped_column(sa.String, nullable=False)
-    photos = mapped_column(sa.ARRAY(sa.String))
+    photos: Mapped[list[str]] = mapped_column(sa.ARRAY(sa.String), nullable=False)
+    website: Mapped[str] = mapped_column(sa.String)
 
-    # TODO: вписать сюда n таблиц
+    ext_serv_link_1: Mapped[str] = mapped_column(sa.String)
+    ext_serv_rank_1: Mapped[float] = mapped_column(sa.Float)
+    ext_serv_num_rev_1: Mapped[int] = mapped_column(sa.Integer)
+
+    ext_serv_link_2: Mapped[str] = mapped_column(sa.String)
+    ext_serv_rank_2: Mapped[float] = mapped_column(sa.Float)
+    ext_serv_num_rev_2: Mapped[int] = mapped_column(sa.Integer)
+
+    ext_serv_link_3: Mapped[str] = mapped_column(sa.String)
+    ext_serv_rank_3: Mapped[float] = mapped_column(sa.Float)
+    ext_serv_num_rev_3: Mapped[int] = mapped_column(sa.Integer)
 
     location = mapped_column(Geometry(geometry_type='POINT', srid=4326), nullable=False)
     address_data = mapped_column(Geometry(geometry_type='GEOMETRY', srid=4326), nullable=False) # TODO: разобраться как сюда вставить geojson
-    categories: Mapped[list[int]] = mapped_column(sa.ARRAY(sa.Integer)) # TODO: вспомнить как писать M:N в этой библе
+    categories: Mapped[list[int]] = relationship()
