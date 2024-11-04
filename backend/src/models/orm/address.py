802ef5752ae9a8811d6100a40d56d61fcaf35f5e
Oleg Shvetsov <oashvetsov@edu.hse.ru>
2024-11-04 20:21:58 +0300

    чекнул документацию и понял как правильно эти связи делать

--- backend/src/models/orm/address.py
+++ backend/src/models/orm/address.py
@@ -6,32 +6,42 @@ from geoalchemy2.types import Geometry
 class City(BaseTable):
     __tablename__ = "city"
     name: Mapped[str]
-    address: Mapped[int] = relationship("Addresses", back_populates="city")
+    address: Mapped["Addresses"] = relationship(back_populates="house")
 
 class District(BaseTable):
     __tablename__ = "district"
     name: Mapped[str]
-    address: Mapped[int] = relationship("Addresses", back_populates="district")
+    address: Mapped["Addresses"] = relationship(back_populates="house")
 
 class Street(BaseTable):
     __tablename__ = "street"
     name: Mapped[str]
-    address: Mapped[int] = relationship("Addresses", back_populates="street")
+    address: Mapped["Addresses"] = relationship(back_populates="house")
 
 class House(BaseTable):
     __tablename__ = "house"
     name: Mapped[str]
-    address: Mapped[int] = relationship("Addresses", back_populates="house")
+    address: Mapped["Addresses"] = relationship(back_populates="house")
 
 
 @as_declarative(metadata=metadata)
 class Addresses(BaseTable):
     __tablename__ = "Address"
-    city: Mapped[int] = relationship("City", back_populates='id')
-    district: Mapped[int] = relationship("District", back_populates='id')
-    street: Mapped[int] = relationship("Street", back_populates='id')
-    house: Mapped[int] = relationship("House", back_populates='id')
+    city: Mapped["City"] = relationship(back_populates='address')
+    city_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("city.id"))
+
+    district: Mapped["District"] = relationship(back_populates='address')
+    district_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("district.id"))
+
+    street: Mapped["Street"] = relationship(back_populates='address')
+    street_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("street.id"))
+
+    house: Mapped["House"] = relationship(back_populates='address')
+    house_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("house.id"))
+
     location = mapped_column(Geometry(geometry_type='POINT', srid=4326), nullable=False)
     __table_args__ = (
-        sa.PrimaryKeyConstraint("city", "district", "street", "house", name='address_id'),
+        sa.PrimaryKeyConstraint("city_id", "district_id", "street_id", "house_id", name='address_id'),
     )
+
+    address_for_user: Mapped["AddressesForUser"] = relationship(back_populates="address")
--- backend/src/models/orm/addresses_for_user.py
+++ backend/src/models/orm/addresses_for_user.py
@@ -6,8 +6,12 @@ from sqlalchemy.orm import mapped_column, Mapped, as_declarative, relationship
 @as_declarative(metadata=metadata)
 class AddressesForUser:
     __tablename__ = "Addresses_for_user"
-    user_id: Mapped[int] = relationship("User", back_populates='id')
-    address_id: Mapped[tuple[int, int, int, int]] = relationship("Addresses", back_populates='address_id')
+    user_id: Mapped[int] = mapped_column(sa.Integer, sa.ForeignKey("user.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="address_for_user")
+
+    address_id: Mapped[tuple[int, int, int, int]] = mapped_column(sa.ForeignKey("Address.address_id"), nullable=False)
+    address: Mapped["Addresses"] = relationship(back_populates="address_for_user")
+
     __table_args__ = (
         sa.PrimaryKeyConstraint("user_id", "address_id", name="address_for_user_id"),
     )
