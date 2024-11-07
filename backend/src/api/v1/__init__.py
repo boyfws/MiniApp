cb591c99ee798d30a3610589ce2c941c66ed0496
Oleg Shvetsov <oashvetsov@edu.hse.ru>
2024-11-08 00:29:11 +0300

    добавил роутер с адресом

--- backend/src/api/v1/__init__.py
+++ backend/src/api/v1/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from src.api.v1.handlers.address.address import address_router
 from src.api.v1.handlers.category.category import category_router
 from src.api.v1.handlers.category.favourite import fav_category_router
 from src.api.v1.handlers.owner import owner_router
@@ -15,3 +16,4 @@ router_v1.include_router(fav_category_router)
 router_v1.include_router(fav_restaurant_router)
 router_v1.include_router(owner_router)
 router_v1.include_router(user_router)
+router_v1.include_router(address_router)
