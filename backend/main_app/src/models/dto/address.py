c8531b7efb8a80ac3a595795e4b06248768f503c
Oleg Shvetsov <oashvetsov@edu.hse.ru>
2024-11-11 19:35:33 +0300

    добил адрес

--- backend/main_app/src/models/dto/address.py
+++ backend/main_app/src/models/dto/address.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
 
 class AddressDTO(BaseModel):
-    ...
+    city: str
+    district: str
+    street: str
+    house: str
 
 class AddressRequest(BaseModel):
-    ...
+    id: int
 
 class AddressResult(BaseModel):
-   ...
+   id: int
--- backend/main_app/src/repository/address/address.py
+++ backend/main_app/src/repository/address/address.py
@@ -12,41 +12,29 @@ class AddressRepo:
             session: AsyncSession,
             model: AddressRequest
     ) -> AddressResult:
-        # result = await session.scalars(
-        #     delete(Address)
-        #     .where(Address.id == model.id)
-        #     # .returning(Address.id)
-        # )
-        return AddressResult() #.model_validate(result, from_attributes=True)
+        stmt = delete(Address).where(Address.id == model.id)
+        await session.execute(stmt)
+        return AddressResult(id=model.id)
 
     @staticmethod
     async def add_address(
             session: AsyncSession,
             model: AddressDTO
     ) -> AddressResult:
-        # city_id = await session.scalars(
-        #     insert(City)
-        #     .values(**{"name": model.city})
-        #     # .returning(City.id)
-        # )
-        # district_id = await session.scalars(
-        #     insert(District)
-        #     .values(**{"name": model.district})
-        #     # .returning(District.id)
-        # )
-        # street_id = await session.scalars(
-        #     insert(Street)
-        #     .values(**{"name": model.street})
-        # )
-        # address_id = await session.scalars(
-        #     insert(Address)
-        #     .values(**{
-        #         "city_id": city_id,
-        #         "district_id": district_id,
-        #         "street_id": street_id,
-        #         "house": model.house,
-        #         "location": model.location
-        #     })
-        #     # .returning(Address.id)
-        # )
-        return AddressResult() # AddressResult.model_validate(address_id, from_attributes=True)
+        city_stmt = insert(City).values(name=model.city).returning(City.id)
+        city_id = await session.execute(city_stmt)
+
+        district_stmt = insert(District).values(name=model.district).returning(District.id)
+        district_id = await session.execute(district_stmt)
+
+        street_stmt = insert(Street).values(name=model.street)
+        street_id = await session.execute(street_stmt)
+
+        address_stmt = insert(Address).values(
+            city_id=city_id,
+            district_id=district_id,
+            street_id=street_id,
+            house=model.house
+        ).returning(Address.id)
+        address_id = await session.execute(address_stmt)
+        return AddressResult.model_validate(address_id.first(), from_attributes=True)
