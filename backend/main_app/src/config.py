c2ffe686e28af425e85593f875a7be0126b659f0
boyfws <minkin.d.d@gmail.com>
2024-11-28 07:18:05 +0300

    Временное решение для стабильного запуска

--- backend/main_app/src/config.py
+++ backend/main_app/src/config.py
@@ -35,7 +35,7 @@ class NGINXConfig:
 @dataclass
 class MongoDBConfig:
     host: Optional[str] = os.getenv("MONGODB_HOST")
-    port: int = os.getenv("MONGODB_PORT", 0)
+    port: int = os.getenv("MONGODB_PORT", 1)
     database: str = os.getenv("MONGO_DATABASE", 'db')
     menu_collection: str = os.getenv("MENU_COLLECTION", 'collection')
     test_menu_collection: str = os.getenv("TEST_MENU_COLLECTION", 'test')
