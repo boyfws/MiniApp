8182f3530348f8fb2b2ef12007c2314f0da64a36
boyfws <minkin.d.d@gmail.com>
2024-12-12 08:53:27 +0300

    Поправил обработку кейса без деф адреса

--- frontend/src/pages/MainPage/utils/LoadDefAddress.js
+++ frontend/src/pages/MainPage/utils/LoadDefAddress.js
@@ -3,25 +3,25 @@ import fetchDefAddress from "../../../api/fetchDefAddress";
 
 const GetLoadDefAddress = (InitDataLoaded, setDefAddress) => () => {
     const fetchData = async () => {
+        let flag = true
         const tg_storage = window.Telegram.WebApp.CloudStorage
-        tg_storage.getItem("last_address", async (err, data) => {
+        tg_storage.getItem("last_address", (err, data) => {
             if (!err) {
                 setDefAddress(data)
+                flag = false
             }
-            else {
-                const city_eng_query = await getUserCity()
-                if (!city_eng_query.error) {
-                    const def_address_query = await fetchDefAddress(city_eng_query.data)
-                    if (!def_address_query.error) {
-                        setDefAddress(def_address_query.data)
-
-                    }
-                }
-
-            }
-
         })
+        if (flag) {
+            const city_eng_query = await getUserCity()
+            if (!city_eng_query.error) {
+                const def_address_query = await fetchDefAddress(city_eng_query.data)
+                if (!def_address_query.error) {
+                    setDefAddress(def_address_query.data)
 
+                }
+            }
+
+        }
     }
     if (InitDataLoaded) {
         fetchData()
