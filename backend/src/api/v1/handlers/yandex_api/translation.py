1a69ff85d849bba351a6664e4c00eb99df53321a
root <root@LAPTOP-2MQ2SEHL.>
2024-12-17 22:15:13 +0300

    добавил ручку с переводом

---
+++ backend/src/api/v1/handlers/yandex_api/translation.py
@@ -0,0 +1,12 @@
+import googletrans
+from googletrans import Translator
+
+
+def get_rus_city(city: str) -> str:
+    city = city.strip().title()
+    try:
+        translator = Translator()
+        translation = translator.translate(city, src='en', dest='ru')
+        return translation.text
+    except Exception as e:
+        return f"Translation error: {e}"
