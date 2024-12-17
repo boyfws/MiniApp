468f2c8b21871d19400139ad3b91181e2f8a5719
boyfws <minkin.d.d@gmail.com>
2024-12-17 22:09:51 +0300

    Маленький фикс

--- frontend/src/api/verificateInitData.js
+++ frontend/src/api/verificateInitData.js
@@ -1,7 +1,7 @@
 
 // Post запрос к серверу
 const verificateInitData = async (initData) => {
-    return {error: false, message: "true"};
+    return {error: false, data: {access_token: 0, refresh_token: 0}};
 }
 
 
