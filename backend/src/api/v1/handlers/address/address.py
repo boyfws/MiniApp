70d132671f25ba0bf8e94675d4d4b5c94033b194
Oleg Shvetsov <oashvetsov@edu.hse.ru>
2024-12-18 23:19:17 +0300

    add address

--- backend/src/api/v1/handlers/address/address.py
+++ backend/src/api/v1/handlers/address/address.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 
+from src.api.v1.handlers.yandex_api.GeoCode import GeoJson
 from src.models.dto.address import AddressResult, AddressDTO, AddressRequest
 from src.service.address import get_address_service
 from src.service.address import AddressService
@@ -11,10 +12,21 @@ address_router = APIRouter(
 
 @address_router.post("/add_address/")
 async def add_address(
-        model: AddressDTO,
+        model: GeoJson,
         service: AddressService = Depends(get_address_service)
 ) -> AddressResult:
-    return await service.add_address(model)
+    address = model.get("properties")
+    coordinates = model.get("geometry").get("coordinates")
+    location = f"SRID=4326;POINT({coordinates[0]} {coordinates[1]})"
+    result = AddressDTO(
+        location=location,
+        region=address.get('region'),
+        city=address.get('street'),
+        district=address.get('district'),
+        street=address.get('street'),
+        house=address.get('house')
+    )
+    return await service.add_address(result)
 
 @address_router.delete("/delete_address/{address_id}")
 async def delete_address(
