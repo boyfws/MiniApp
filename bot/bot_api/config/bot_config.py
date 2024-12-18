104f657cd2f75bb727b4ede4c4c490dc8c67317c
boyfws <minkin.d.d@gmail.com>
2024-12-18 08:02:37 +0300

    Добавил линку на миниапп

--- bot/bot_api/config/bot_config.py
+++ bot/bot_api/config/bot_config.py
@@ -9,10 +9,8 @@ load_dotenv()
 MAX_INLINE_BUTTON_LEN = 15
 
 TOKEN: str = cast(str, os.getenv('BOT_TOKEN'))
-'''
-Можно юзать https://www.temporary-url.com/ для временных ссылок для теста
-'''
-url: str = "https://www.temporary-url.com/171"  #An HTTPS URL
+
+url: str = "https://mini-app-test-prikol.duckdns.org/main"  #An HTTPS URL
 web_app_info: WebAppInfo = WebAppInfo(url)
 
 
