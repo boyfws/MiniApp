db4fa09137de22b1514350a3d629d9d215cf3d24
Anna Lyndina <lyndina.anna@MacBook-Air-Anna.local>
2024-12-18 04:00:24 -0800

    fix get all addresses

--- backend/src/service/address/address_for_user.py
+++ backend/src/service/address/address_for_user.py
@@ -1,10 +1,13 @@
 from src.database.sql_session import get_session
 from src.models.dto.address_for_user import AddressForUserDTO, AddressesResponse, AllAddressesForUser
 from src.repository.address.address_for_user import AddressForUserRepo
+from src.repository.address.address import AddressRepo
+from src.models.dto.address import AddressDTO
 
 
 class AddressesForUserService:
     def __init__(self, session_getter=get_session) -> None:
+        self.address_repo = AddressRepo(session_getter)
         self.repo = AddressForUserRepo(session_getter)
 
     async def delete(self, model: AddressForUserDTO) -> AddressesResponse:
@@ -16,8 +19,14 @@ class AddressesForUserService:
     async def get_all_user_fav_restaurants(
             self,
             model: AllAddressesForUser
-    ) -> list[AddressForUserDTO]:
-        return await self.repo.get_all_user_addresses(model)
+    ) -> list[AddressDTO]:
+        addresses = await self.repo.get_all_user_addresses(model)
+        addresses_geo = []
+        # теперь надо для каждого адреса сделать запрос и получить пропертис
+        for address in addresses:
+            addresses_geo.append(self.address_repo.get(address.address_id))
+            
+        return addresses_geo
 
     async def drop_all_user_fav_restaurants(
             self,
