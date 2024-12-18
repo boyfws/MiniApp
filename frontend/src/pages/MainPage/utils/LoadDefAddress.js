64dcd2565e3ac4ba7aa331f945ac4adf13c9cd51
boyfws <minkin.d.d@gmail.com>
2024-12-18 10:57:27 +0300

    Улучшил обрабаотку веб хрранилища

--- frontend/src/pages/MainPage/utils/LoadDefAddress.js
+++ frontend/src/pages/MainPage/utils/LoadDefAddress.js
@@ -3,7 +3,6 @@ import fetchDefAddress from "../../../api/fetchDefAddress";
 
 const GetLoadDefAddress = (InitDataLoaded, setDefAddress) => () => {
     const fetchData = async () => {
-        let flag = true
         try {
             const tg_storage = window.Telegram.WebApp.CloudStorage
             const data = await new Promise((resolve, reject) => {
@@ -15,23 +14,25 @@ const GetLoadDefAddress = (InitDataLoaded, setDefAddress) => () => {
                     }
                 });
             });
+            if (data === "") {
+                throw new Error("Пустой ключ")
+            }
+            else {
+                setDefAddress(data);
+            }
 
-            setDefAddress(data);
-            flag = false;
+
         } catch (err) {
-            // Обработка ошибки
-        }
-        if (flag) {
             const city_eng_query = await getUserCity()
             if (!city_eng_query.error) {
                 const def_address_query = await fetchDefAddress(city_eng_query.data)
                 if (!def_address_query.error) {
                     setDefAddress(def_address_query.data)
-
                 }
             }
 
         }
+
     }
     if (InitDataLoaded) {
         fetchData()
