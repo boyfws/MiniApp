13b2e36e249a45ff5d49af2fcc62097a1375219d
Oleg Shvetsov <oashvetsov@edu.hse.ru>
2024-12-19 18:11:38 +0300

    забыл что в преобразовании юзается другой класс

--- backend/src/service/address/__init__.py
+++ backend/src/service/address/__init__.py
@@ -1,8 +1,7 @@
 from src.repository.address.address import AddressRepo
 from .address import AddressService
 from .address_for_user import AddressesForUserService
-from ...api.v1.handlers.yandex_api.GeoCode import GeoJson
-from ...models.dto.address import AddressDTO
+from ...models.dto.address import AddressDTO, GeoJson
 
 
 def get_address_service() -> AddressService:
@@ -12,8 +11,9 @@ def get_address_for_user_service() -> AddressesForUserService:
     return AddressesForUserService()
 
 def transform_to_dto(model: GeoJson) -> AddressDTO:
-    props = model.get("properties")
-    coordinates = model.get("geometry").get("coordinates")
+    model_dict = model.model_dump()
+    props = model_dict.get("properties")
+    coordinates = model_dict.get("geometry").get("coordinates")
     location = f"SRID=4326;POINT({coordinates[0]} {coordinates[1]})"
     result = AddressDTO(
         location=location,
