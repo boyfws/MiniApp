01c0a8c840aa1ecfce50342cf584c2d61e413ac0
boyfws <minkin.d.d@gmail.com>
2024-12-19 16:01:51 +0300

    Фикс запроса на регаистрацию нового адреса

--- frontend/src/api/sendAddAddress.js
+++ frontend/src/api/sendAddAddress.js
@@ -5,14 +5,7 @@ const sendAddAddress = async (new_address, id) => {
     const retries = 4
     const delay = 5
 
-    const filteredAddress = {
-        ...new_address,
-        properties: Object.fromEntries(
-            Object.entries(new_address.properties).filter(([key, value]) => value !== null)
-        )
-    };
-
-    return await POST_query(url, filteredAddress, {}, retries, delay)
+    return await POST_query(url, new_address, {}, retries, delay)
 
 
 }
