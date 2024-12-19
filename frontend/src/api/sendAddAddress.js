30ecd397bef46a72c84807175bba4091ad9ba76a
boyfws <minkin.d.d@gmail.com>
2024-12-19 15:18:50 +0300

    Добавление фикса для клика на адрес в рекмеонациях

--- frontend/src/api/sendAddAddress.js
+++ frontend/src/api/sendAddAddress.js
@@ -2,13 +2,9 @@ import POST_query from "./queries/POST_query";
 
 const sendAddAddress = async (new_address, id) => {
     const url = `/v1/AddressesForUser/add_address/${id}`;
-    const options = {
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(new_address),
-    }
     const retries = 4
     const delay = 5
-    return await POST_query(url, options, retries, delay)
+    return await POST_query(url, new_address, {}, retries, delay)
 
 
 }
