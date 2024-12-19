eb0b984e17510fc5db73aae1a61bc7d79d06e297
boyfws <minkin.d.d@gmail.com>
2024-12-19 15:35:30 +0300

    Пробуем фиксить баг с отправкой нового адреса на бэк

--- frontend/src/api/sendAddAddress.js
+++ frontend/src/api/sendAddAddress.js
@@ -4,7 +4,15 @@ const sendAddAddress = async (new_address, id) => {
     const url = `/v1/AddressesForUser/add_address/${id}`;
     const retries = 4
     const delay = 5
-    return await POST_query(url, new_address, {}, retries, delay)
+
+    const filteredAddress = {
+        ...new_address,
+        properties: Object.fromEntries(
+            Object.entries(new_address.properties).filter(([key, value]) => value !== null)
+        )
+    };
+
+    return await POST_query(url, filteredAddress, {}, retries, delay)
 
 
 }
