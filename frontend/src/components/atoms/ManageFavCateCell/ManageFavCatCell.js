580b0f60ec62853acf778c0b4f163c815eb8ab59
boyfws <minkin.d.d@gmail.com>
2024-12-22 03:38:11 +0300

    Фикс бага при добавлении приложения на главный экран

---
+++ frontend/src/components/atoms/ManageFavCateCell/ManageFavCatCell.js
@@ -0,0 +1,1 @@
+import './ManageFavCatCell.css'
--- frontend/src/components/molecules/ListOfOptionsSettings/utills/GetGetHomeScreenStatus.js
+++ frontend/src/components/molecules/ListOfOptionsSettings/utills/GetGetHomeScreenStatus.js
@@ -1,9 +1,12 @@
 const GetGetHomeScreenStatus = (setShowAddToHomeScreen) => () => {
     try {
         const tg = window.Telegram.WebApp;
-        new Promise(resolve => tg.checkHomeScreenStatus(
-            (status) => resolve(status))
-        ).then(
+        new Promise((resolve) => {
+            tg.checkHomeScreenStatus(
+            (status) => {resolve(status)}
+            );
+        }
+    ).then(
             status => {
                 console.log(status);
                 setShowAddToHomeScreen((status === 'unknown' || status === 'missed') && parseFloat(tg.version) >= 8);
