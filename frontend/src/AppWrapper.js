ace786c0607e2397dd30a59a3871b609d9dc8b0a
boyfws <minkin.d.d@gmail.com>
2024-12-23 01:20:53 +0300

    Для переноса в prod

--- frontend/src/AppWrapper.js
+++ frontend/src/AppWrapper.js
@@ -1,12 +1,34 @@
+// Css
+import '@telegram-apps/telegram-ui/dist/styles.css'; // Глобальные стили из библиотеки
+
+// Ext lib
+import { AppRoot } from "@telegram-apps/telegram-ui";
+import React, {useEffect, useState} from "react";
+
+// State
 import InitDataStateStore from "./state_management/stores/InitDataLoadingState";
+
+
 import GetinitializeTelegram from "./utils/telegramInit";
-import React, {useEffect} from "react";
 import App from "./App";
 
 const AppWrapper = ({}) => {
     const {setInitDataLoaded} = InitDataStateStore()
 
+    const [isDark, setIsDark] = useState(true);
+    const [platform, setPlatform] = useState("base");
+
     const initTG = GetinitializeTelegram(setInitDataLoaded);
+
+
+    useEffect(() => {
+        setIsDark(window.Telegram.WebApp.colorScheme === 'dark');
+    }, [])
+
+    useEffect(() => {
+        setPlatform(window.Telegram.WebApp.platform)
+    }, [])
+
 
 
     useEffect(initTG, [])
@@ -17,7 +39,12 @@ const AppWrapper = ({}) => {
 
 
     return (
-        <App />
+        <AppRoot
+            appearance={isDark ? 'dark' : 'light'}
+            platform={['macos', 'ios'].includes(platform) ? 'ios' : 'base'}
+        >
+            <App />
+        </AppRoot>
     )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '@telegram-apps/telegram-ui/dist/styles.css'; // Глобальные стили из библиотеки
-import { AppRoot } from '@telegram-apps/telegram-ui';
 import { ContextProvider } from './state_management/context/Context';
 import AppWrapper from './AppWrapper'
 
@@ -13,9 +11,7 @@ document.body.style.backgroundColor = 'var(--tgui--bg_color)';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ContextProvider>
-        <AppRoot>
-            <AppWrapper />
-        </AppRoot>
+        <AppWrapper />
     </ContextProvider>,
 
 );
