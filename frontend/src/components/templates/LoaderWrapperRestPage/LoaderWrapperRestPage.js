ab076225a718728155f67fb20c0188941d950a02
boyfws <minkin.d.d@gmail.com>
2024-12-23 02:13:46 +0300

    Дебажим лоадер на RestPage

--- frontend/src/components/templates/LoaderWrapperRestPage/LoaderWrapperRestPage.js
+++ frontend/src/components/templates/LoaderWrapperRestPage/LoaderWrapperRestPage.js
@@ -1,14 +1,22 @@
 // Comp
 import Loader from "../../molecules/Loading/Loading";
 
+// Ext lib
+import React, { useState, useEffect } from "react";
+
 // Utils
 import onFinishRestPage from './utils/onFinishRestPage'
 
 
 const LoaderWrapper = ({RestDataLoading, MenuLoading, setShowContent}) => {
     const onFinish = onFinishRestPage(setShowContent);
+    const [loading, setLoading] = useState(true);
 
-    const loading = RestDataLoading || MenuLoading
+
+    useEffect(() => {
+        setLoading(RestDataLoading || MenuLoading);
+    }, [RestDataLoading, MenuLoading]);
+
 
     return (
         <Loader loading={loading} onFinish={onFinish}/>
--- frontend/src/components/templates/LoaderWrapperRestPage/utils/onFinishRestPage.js
+++ frontend/src/components/templates/LoaderWrapperRestPage/utils/onFinishRestPage.js
@@ -1,5 +1,6 @@
 const onFinishRestPage = (setShowContent) => () =>  {
     setShowContent(true);
+    console.log("Выполнилась команда показать контент на фронте");
 }
 
 export default onFinishRestPage;
