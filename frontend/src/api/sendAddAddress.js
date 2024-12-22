70856eb47c284770df083f708dece479af17d516
boyfws <minkin.d.d@gmail.com>
2024-12-22 07:44:14 +0300

    Перевел на сущесвтующий метод

--- frontend/src/api/sendAddAddress.js
+++ frontend/src/api/sendAddAddress.js
@@ -1,11 +1,11 @@
-import POST_query from "./queries/POST_query";
+import postWithRetry from "./queries/POST_query";
 
 const sendAddAddress = async (new_address, id) => {
     const url = `/api/v1/AddressesForUser/add_address/${id}`;
     const retries = 4
     const delay = 5
 
-    return await POST_query(url, new_address, {}, retries, delay)
+    return await postWithRetry(url, new_address, {}, retries, delay)
 
 
 }
