676ca1103429145bd2600cde1a1af4b9be273027
boyfws <minkin.d.d@gmail.com>
2024-12-23 04:38:41 +0300

    Фикс баги

--- frontend/src/api/fetchMenuForRest.js
+++ frontend/src/api/fetchMenuForRest.js
@@ -1,7 +1,7 @@
 import GET_query from "./queries/GET_query";
 
 const fetchMenuForRest = async (id) => {
-    const url = `/api/v1/Menu/get_menu_by_rest_id/${id}'`
+    const url = `/api/v1/Menu/get_menu_by_rest_id/${id}`
     const ret = await GET_query(url, {}, 2, 10)
 
     console.log(ret)
