03898dc334793ea2e4b0492bf050c8bdfab7f5e6
boyfws <minkin.d.d@gmail.com>
2024-12-23 05:56:47 +0300

    Тест говна 4

--- frontend/src/pages/RestaurantPage/utils/LoadRestData.js
+++ frontend/src/pages/RestaurantPage/utils/LoadRestData.js
@@ -4,7 +4,7 @@ const GetLoadRestData = (setLoading, id, setRestData) => () =>  {
     async function fetchData() {
         const user_id = sessionStorage.getItem("userId");
         const rest_data_query =  await fetchRestaurnatInfo(id, user_id)
-
+        console.log(rest_data_query)
         if (!rest_data_query.error) {
             setRestData(rest_data_query.error);
             setLoading(false);
