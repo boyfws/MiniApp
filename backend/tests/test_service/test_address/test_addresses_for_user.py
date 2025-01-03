e143cdfdec410aa4544ff25f488ed9584365448e
Oleg Shvetsov <oashvetsov@edu.hse.ru>
2025-01-04 02:38:10 +0300

    тесты на сервис адресов

--- backend/tests/test_service/test_address/test_addresses_for_user.py
+++ backend/tests/test_service/test_address/test_addresses_for_user.py
@@ -1,8 +1,10 @@
 import pytest
 from sqlalchemy import text
 
+from src.models.dto.address import AddressDTO
 from src.models.dto.address_for_user import AddressForUserDTO, AllAddressesForUser
 from src.service.address import AddressesForUserService
+from tests.common.address import get_addresses
 from tests.sql_connector import get_session_test
 from tests.test_repository.test_address.test_addresses_for_user import create_db_values_1, create_db_values_2
 
@@ -22,33 +24,66 @@ async def truncate_db():
 
 
 @pytest.mark.parametrize(
-    "model, expected_status",
+    "user_id, model, expected_status",
     [
-        (AddressForUserDTO(user_id=1, address_id=1), 200),
-        (AddressForUserDTO(user_id=1, address_id=2), 200)
+        (1, get_addresses()[1], 200),
+        (1, get_addresses()[2], 200),
     ]
 )
-async def test_create(model: AddressForUserDTO, expected_status: int, create_db_values_1, truncate_db):
-    result = await address_for_user_service.create(model)
+async def test_create_if_address_exists(
+        user_id: int,
+        model: AddressDTO,
+        expected_status: int,
+        create_db_values_1, truncate_db
+):
+    result = await address_for_user_service.create(user_id, model)
     assert result.status == expected_status
 
+    all_addresses = await address_for_user_service.repo.get_all_user_addresses(AllAddressesForUser(user_id=user_id))
+    addresses_geo = []
+    for address in all_addresses:
+        addresses_geo.append(await address_for_user_service.address_repo.get(address.address_id))
+    assert addresses_geo == [model]
 
 @pytest.mark.parametrize(
-    'model, expected_status',
+    "user_id, model, expected_status",
     [
-        (AddressForUserDTO(user_id=1, address_id=1), 200),
-        (AddressForUserDTO(user_id=1, address_id=2), 200)
+        (1, get_addresses()[1], 200),
+        (1, get_addresses()[2], 200),
     ]
 )
-async def test_delete(model: AddressForUserDTO, expected_status: int, create_db_values_1, truncate_db):
-    result = await address_for_user_service.delete(model)
+async def test_create_if_address_new(
+        user_id: int,
+        model: AddressDTO,
+        expected_status: int,
+        truncate_db
+):
+    result = await address_for_user_service.create(user_id, model)
     assert result.status == expected_status
 
+    all_addresses = await address_for_user_service.repo.get_all_user_addresses(AllAddressesForUser(user_id=user_id))
+    addresses_geo = []
+    for address in all_addresses:
+        addresses_geo.append(await address_for_user_service.address_repo.get(address.address_id))
+    assert addresses_geo == [model]
+
+
+
+# @pytest.mark.parametrize(
+#     'model, expected_status',
+#     [
+#         (AddressForUserDTO(user_id=1, address_id=1), 200),
+#         (AddressForUserDTO(user_id=1, address_id=2), 200)
+#     ]
+# )
+# async def test_delete(model: AddressForUserDTO, expected_status: int, create_db_values_1, truncate_db):
+#     result = await address_for_user_service.delete(model)
+#     assert result.status == expected_status
 
 @pytest.mark.parametrize(
     "model, expected_list_result",
     [
-        (AllAddressesForUser(user_id=1), [AddressForUserDTO(user_id=1, address_id=1), AddressForUserDTO(user_id=1, address_id=2)]),
+        (AllAddressesForUser(user_id=1), [get_addresses()[1], get_addresses()[2]]),
         (AllAddressesForUser(user_id=1000), [])
     ]
 )
@@ -56,7 +91,7 @@ async def test_get_all_user_addresses(
         model: AllAddressesForUser,
         expected_list_result: list[AddressForUserDTO],
         create_db_values_2, truncate_db): # fixtures
-    result = await address_for_user_service.get_all_user_fav_restaurants(model)
+    result = await address_for_user_service.get_all_user_addresses(model)
     assert result == expected_list_result
 
 @pytest.mark.parametrize(
